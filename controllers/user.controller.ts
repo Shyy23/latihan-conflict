a47a52640780731c376431208570504fd65d1622
AchmadAndhika <andikanatanegara1@gmail.com>
2026-02-17 22:52:47 +0700

    feat: add getAllusers

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,3 +1,36 @@
+import type { Context } from "hono";
+import { ClassController } from "./class.controller";
+import { UserModel } from "../models/user.model";
+
 export class UserController {
-    
+  async getAllusers(c: Context) {
+    const users = await UserModel.fetchAll({
+      withRelated: ["schools", "classess"],
+    }).map((item: any) => {
+      const itemJson = item.toJson();
+
+      const classRel = item.related("classes");
+      const schoolRel = item.related("schools");
+
+      return {
+        id: itemJson.id,
+        name: itemJson.name,
+        gender: itemJson.gender,
+        address: itemJson.address,
+        school: {
+          id: schoolRel.get("id"),
+          name: schoolRel.get("name"),
+        },
+        class: {
+          id: classRel.get("id"),
+          name: classRel.get("name"),
+        },
+      };
+    });
+    return c.json({
+      status: true,
+      message: "User data successfully displayed",
+      data: users,
+    });
+  }
 }
