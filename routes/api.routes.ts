0888158bd0957ac05850695e08803552c42210ff
shyy23 <syahrulhidayatulloh14@gmail.com>
2026-02-17 22:20:44 +0700

    delete user route

--- routes/api.routes.ts
+++ routes/api.routes.ts
@@ -2,7 +2,5 @@ import { Hono } from "hono";
 import { UserController } from "../controllers/user.controller";
 const api = new Hono();
 
-const userController = new UserController();
-api.get("/users", userController.getAllUsers);
-api.get("/user/:id", userController.getUserDetail);
+
 export default api;
